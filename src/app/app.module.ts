9bb3135193ad4f07c1fbf5c314aa873dcbabb53e
SabtoHaryadi <chiefman12345@gmail.com>
2021-12-07 19:47:52 +0700

    web-soccer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScoringsComponent } from './scorings/scorings.component';
+import { AdminComponent } from './admin/admin.component';
+import { StandingsComponent } from './standings/standings.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScoringsComponent,
+    AdminComponent,
+    StandingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
